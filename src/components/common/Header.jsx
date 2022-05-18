67c88abdef472e03278b75dfda6baa7538599c9e
Teshome Kurabachew <dear.teshe@gmail.com>
2022-05-18 13:40:51 +0300

    Add Header for home and detail page

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,13 +1,18 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
+import { FaUniversity } from 'react-icons/fa';
 import styles from './Header.module.scss';
 
 function Header({ name, view }) {
-  const { header, image, headerTitle } = styles;
+  const {
+    header, image, headerTitle, icon,
+  } = styles;
 
   return (
     <div className={header}>
-      <div className={image} />
+      <div className={image}>
+        <FaUniversity className={icon} />
+      </div>
       <div className={headerTitle}>
         <h2>{name}</h2>
         <p>{view}</p>
